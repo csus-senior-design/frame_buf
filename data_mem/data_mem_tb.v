c7e26a87f6c1df2a042668b0b7e40f0d00a4e084
P4T4R <guitarisrockin@hotmail.com>
2015-03-08 17:10:07 -0700

    Changes to data_mem_tb.v

--- data_mem/data_mem_tb.v
+++ data_mem/data_mem_tb.v
@@ -3,22 +3,22 @@
 module memory_tb;
   reg clk, wr_en, rd_en, reset;
   reg [2:0] wr_addr, rd_addr;
-  reg [7:0] wr_data;
-  wire [7:0] rd_data;
+  reg [15:0] wr_data;
+  wire [15:0] rd_data;
 
   data_mem #(.DATA_WIDTH(16), .ADDR_WIDTH(3))
            uut (.clk(clk), .wr_en(wr_en), .rd_en(rd_en), .reset(reset),
             .wr_addr(wr_addr), .rd_addr(rd_addr), .wr_data(wr_data),
             .rd_data(rd_data));
             
-  always #10 clk = ~clk;
+  always #20 clk = ~clk;
 
   initial begin
     $monitor("wr_addr: %h, wr_data: %h", wr_addr, wr_data);
     $monitor("rd_addr: %h, rd_data: %h", rd_addr, rd_data);
     
     clk = 1'b0;
-    wr_data = 16'h01;
+    wr_data = 16'h1;
     wr_en = 1'b1;
     rd_en = 1'b1;
     reset = 1'b0;
@@ -26,24 +26,23 @@ module memory_tb;
     rd_addr = 3'h0;
 
     #5 wr_en = 1'b0;
+    reset = 1'b1;
 
-    #10 reset = 1'b1;
-
-    #20 wr_addr = wr_addr + 3'h1;
+    #15 wr_addr = wr_addr + 3'h1;
     wr_data = 16'h02;
 
-    #20 wr_addr = wr_addr + 3'h1;
+    #15 wr_addr = wr_addr + 3'h1;
     wr_data = 16'h03;
 
-    #20 wr_addr = wr_addr + 3'h1;
+    #15 wr_addr = wr_addr + 3'h1;
     wr_data = 16'h04;
     rd_en = 1'b0;
 
-    #20 rd_addr = rd_addr + 3'h1;
+    #15 rd_addr = rd_addr + 3'h1;
 
-    #20 rd_addr = rd_addr + 3'h1;
+    #15 rd_addr = rd_addr + 3'h1;
 
-    #20 rd_addr = rd_addr + 3'h1;
+    #15 rd_addr = rd_addr + 3'h1;
 
     #10 $finish;
   end
